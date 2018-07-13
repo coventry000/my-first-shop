cd902df2cd0c218f75000772d48fbe8e52a0522b
admin <something@gmail.com>
2018-07-13 19:22:40 +0200

    My first commit

---
+++ shop/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    platform = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    image = models.ImageField()
+    price = models.FloatField()
+    description = models.TextField()
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.title
+
